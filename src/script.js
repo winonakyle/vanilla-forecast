a69ef74558b257209b600458e063c6e46be5801e
Winona Kyle <winonaksb@gmail.com>
2025-02-06 16:45:54 -0600

    finishing touch

--- src/script.js
+++ src/script.js
@@ -67,7 +67,7 @@ function displayForecast(response) {
 let forecastHtml = "";
 
 response.data.daily.forEach(function (day, index){
-  if (index < 6) {
+  if (index < 5) {
   forecastHtml = forecastHtml +
     `
     <div class="weather-forecast-day-wrap">
